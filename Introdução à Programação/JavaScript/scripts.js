9e66f80fa5bd2c0d3d7830e57eae1b3000c676ba
Diego Ramon <32984626+DihhRamon@users.noreply.github.com>
2024-02-13 10:06:38 -0300

    Upload pasta JavaScript

---
+++ Introdução à Programação/JavaScript/scripts.js
@@ -0,0 +1,72 @@
+// const a = 10;
+
+// console.log(a)
+
+// let nota = 6;
+
+ 
+
+// if(nota >= 8){
+//     console.log("Ótimo trabalho!");
+// } else if(nota >= 6){
+//     console.log("Bom trabalho");
+// } else {
+//     console.log("Precisa melhorar");
+// }
+
+
+// post02
+// formulario
+// section
+// data dos posts
+// textos dos post
+
+
+
+
+// const listItens = document.getElementsByTagName("li");
+
+// const post = document.getElementById("post02");
+
+// console.log(post)
+
+// const products = document.getElementsByClassName("product");
+
+// console.log(post01.innerText)
+
+
+
+// Post 02
+// const post = document.getElementById("post02");
+// console.log(post.innerText)
+
+// Formulário
+// const form = document.getElementById("formulario");
+// console.log(form.innerText)
+
+// Texto Posts
+// const posts = document.getElementsByClassName("post-texto");
+
+// console.log(posts[0].innerText)
+// console.log(posts[1].innerText)
+
+
+// Data dos posts
+// const dates = document.getElementsByClassName("post-data");
+
+// console.log(dates[0].innerText)
+// console.log(dates[1].innerText)
+
+
+
+
+// O link dentro do texto do primeiro post
+// A palavra em negrito dentro do texto do segundo post
+// O input de nome do formulário
+// Os links da lista de redes no final da página
+// Os links da navegação (só os links, não os elementos de lista)
+// Os 4 "Autor:" e "Data:" em negrito nos dois posts'
+
+const post1 = document.querySelectorAll("main section article[2] #post02 strong")
+
+console.log(post1.innerText)
